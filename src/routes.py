d970f6273e364b4c28f26525e071a67fcfacaef2
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-06-10 17:21:30 +0530

    U: fix import

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,6 @@
 from flask import request, send_file, jsonify, render_template, redirect, url_for, request, flash
 from src import app
-from procLogic import processVideoOperation
+from .procLogic import processVideoOperation
 
 @app.route('/')
 def index():
